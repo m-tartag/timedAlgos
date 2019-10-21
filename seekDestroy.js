debf854042e0c103769c8d4c7622e15ff614c57b
m-tartag <mattbrs51@gmail.com>
2019-10-21 10:40:25 -0400

    finished seek and destroy"

---
+++ seekDestroy.js
@@ -0,0 +1,8 @@
+function destroyer(arr) {
+  const ARGS = [...arguments].splice(1);
+  return arr.filter(item => !ARGS.includes(item));
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+// 6.2 minutes
