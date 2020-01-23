fbcdf9610a01ececff2b0a21dda907e9a981960f
m-tartag <mattbrs51@gmail.com>
2020-01-22 22:35:37 -0500

    seek algo

---
+++ newnew.js
@@ -0,0 +1,9 @@
+const NAMES = ['Matt', 'Harrison', 'Sean', 'Sarah'];
+
+function ScrambleNames(name) {
+  for (let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+  }
+}
+
+console.log(ScrambleNames(NAMES));
